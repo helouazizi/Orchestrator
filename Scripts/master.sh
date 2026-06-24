75dc8ecba71c0cb3ecbba7d63899408fa5dfe5b6
ismailsayen <ismailsvn02@gmail.com>
2026-06-24 14:02:45 +0100

    Implement dynamic environment variable substitution
    Refactors the deployment scripts to automatically inject variables from an environment file into Kubernetes manifests using envsubst. This replaces hardcoded secrets with a more secure, template-based approach.
    
    Consolidates the RabbitMQ configuration by merging the secret definition into the pod manifest and updating service ports to match the management interface.

--- Scripts/master.sh
+++ Scripts/master.sh
@@ -6,8 +6,23 @@ if [ ! -f /etc/rancher/k3s/k3s.yaml ]; then
     curl -sfL https://get.k3s.io | sh -
     sudo chmod 644 /etc/rancher/k3s/k3s.yaml
     sudo cat /var/lib/rancher/k3s/server/node-token > /vagrant/node-token
-    sudo kubectl create secret generic app-secret --from-env-file=/vagrant/.env
+
+    # search for all yaml files to extract variables from
+
+    files=$(find /vagrant/Manifests -name "*.yml" -o -name "*.yaml")
+
+    for file in $files; do
+        # search for all variables in the yaml file and check if they are defined in the .env file
+        while read -r var; do
+            envs=$(grep "^${var}=" /vagrant/.env)
+            export $envs | xargs && envsubst < $file | sudo kubectl apply -f -
+            break
+        done < <(grep -oP '(?<=\$)\w+' "$file")
+    done
+
+
+
+    sudo kubectl apply -f /vagrant/Manifests/rabbitmq_pod.yml
 fi
-sudo kubectl apply -f /vagrant/Manifests/rabbitmq_pod.yml
 
 
--- Scripts/test.sh
+++ Scripts/test.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 set -e
+# search for all yaml files to extract variables from
 
-#upload environment variables
-source ../.env
+files=$(find ../Manifests -name "*.yml" -o -name "*.yaml")
+
+for file in $files; do
+    # search for all variables in the yaml file and check if they are defined in the .env file
+    while read -r var; do
+        envs=$(grep "^${var}=" ../.env)
+        export $envs | xargs && envsubst < $file
+        break
+    done < <(grep -oP '(?<=\$)\w+' "$file")
+done
 
 
-cat << end > /tmp/temp_envs/.env.rabbitmq
-RABBITMQ_USER=$RABBITMQ_USER
-RABBITMQ_PASS=$RABBITMQ_PASS
-RABBITMQ_VHOST=$RABBITMQ_VHOST
-end
