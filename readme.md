7282c57506b616963c30d93fee45d7c4f8cca94b
ismailsayen <ismailsvn02@gmail.com>
2026-06-28 17:07:11 +0100

    merge with main

--- readme.md
+++
@@ -1,2 +0,0 @@
-crictl
-kubectl port-forward deployment/api-gateway-app 3000:3000 --address=0.0.0.0
--- srcs/api-gateway-app/app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from .route import services_bp
-
-
-def create_app():
-    app = Flask(__name__)
-    app.register_blueprint(services_bp)
-    
-    return app
